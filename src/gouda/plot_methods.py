a0ce7c0522c801d4a4d7332d8b176ddbf40b8223
Mullans <sean@seanmullan.com>
2021-03-31 14:16:58 -0500

    improve plot flexibility

--- src/gouda/plot_methods.py
+++ src/gouda/plot_methods.py
@@ -3,21 +3,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_accuracy_curve(acc, thresh):
-    """Plot the accuracy curve for the given accuracy and threshold values.
+def plot_accuracy_curve(acc, thresh, label_height=0.5, line_args={}, thresh_args={}):
+    """Plot the accuracy for the given accuracy and threshold values.
 
     Parameters
     ----------
-    acc: np.ndarray
-        An array of the accuracy values for each threshold.
-    thresh: np.ndarray
-        An array of the threshold values to plot
+    acc : numpy.ndarray
+        An array of accuracy values for each given threshold
+    thresh : numpy.ndarray
+        An array of threshold values matching each accuracy value
+    label_height : float
+        The value between 0 and 1 for the y-position of the threshold label on the graph. Set to None for no label (default is 0.5)
+    line_args : dict
+        A dictionary of keyword arguments to pass when plotting the accuracy line
+    thresh_args : dict
+        A dictionary of keyword arguments to pass when plotting the threshold vline
     """
-    plt.plot(thresh, acc)
+    line_defaults = {'color': 'black', 'label': 'accuracy'}
+    for key in line_defaults:
+        if key not in line_args:
+            line_args[key] = line_defaults[key]
+    thresh_defaults = {'color': 'r', 'label': 'best threshold'}
+    for key in thresh_defaults:
+        if key not in thresh_args:
+            thresh_args[key] = thresh_defaults[key]
+
     best_idx = np.argmax(acc)
-    plt.vlines(thresh[best_idx], 0, 1, color='r')
+    best_thresh = thresh[best_idx]
+    best_acc = acc[best_idx]
+
+    plt.plot(thresh, acc, **line_args)
+    plt.vlines(best_thresh, 0, 1, **thresh_args)
     plt.ylim(0, 1)
     plt.xlim(0, 1)
-    plt.text(thresh[best_idx] + 0.01, 0.5, "{:.2f}%".format(acc[best_idx] * 100))
-    plt.xlabel("Prediction Threshold")
-    plt.ylabel("Accuracy")
+    if label_height is not None:
+        plt.text(best_thresh + 0.01, label_height, "{:.2f}% @ {:.2f}".format(best_acc * 100, best_thresh))
+    plt.xlabel('Prediction Threshold')
+    plt.ylabel('Accuracy')
