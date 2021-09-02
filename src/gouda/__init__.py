ba6bb51cade3900d6fcbada5aa44d8c0dc6340a6
Sean Mullan <sean@seanmullan.com>
2021-09-02 10:16:12 -0500

    add general methods

--- src/gouda/__init__.py
+++ src/gouda/__init__.py
@@ -4,10 +4,11 @@ from pkg_resources import DistributionNotFound, get_distribution
 
 from .binaryconfusionmatrix import BinaryConfusionMatrix  # noqa: F401
 from .confusionmatrix import ConfusionMatrix  # noqa: F401
-from .constants import *
+from .constants import *  # noqa: F401, F403
 from .data_methods import *  # noqa: F401, F403
 from .display import print_grid, print_image  # noqa: F401
 from .file_methods import *  # noqa: F401, F403
+from .general import *  # noqa: F401, F403
 from .goudapath import GoudaPath  # noqa: F401
 from .moving_stats import *  # noqa: F401, F403
 
---
+++ src/gouda/general.py
@@ -0,0 +1,49 @@
+"""General purpose methods that don't fit other categories"""
+
+
+def getattr_recursive(item, attr_string):
+    """getattr for nested attributes
+
+    Parameters
+    ----------
+    item : object
+        Any python object with attributes
+    attr_string : string
+        A string of attributes separated by periods (ex: first.second)
+
+    Note
+    ----
+    An example would be a module with submodules: `getattr_recursive(os, 'path.basename')` would return os.path.basename
+    """
+    nested_type = type(item).__name__
+    cur_item = item
+    for key in attr_string.split('.'):
+        try:
+            cur_item = getattr(cur_item, key)
+            nested_type += '.' + type(cur_item).__name__
+        except AttributeError:
+            raise AttributeError("'{}' object has no attribute '{}'".format(nested_type, key))
+    return cur_item
+
+
+def hasattr_recursive(item, attr_string):
+    """hasattr for nested attributes
+
+    Parameters
+    ----------
+    item : object
+        Any python object with attributes
+    attr_string : string
+        A string of attributes separated by periods (ex: first.second)
+
+    Note
+    ----
+    An example would be a module with submodules: `hasattr_recursive(os, 'path.basename')` would return True
+    """
+    cur_item = item
+    for key in attr_string.split('.'):
+        try:
+            cur_item = getattr(cur_item, key)
+        except AttributeError:
+            return False
+    return True
