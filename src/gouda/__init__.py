263d8f823dbbc2a971732806386d85af0c93a2dc
Sean Mullan <sean@seanmullan.com>
2025-03-16 16:03:39 -0500

    start adding ruff, start removing 3.8 support

--- src/gouda/__init__.py
+++ src/gouda/__init__.py
@@ -1,10 +1,5 @@
-import sys
-
-if sys.version_info[:2] >= (3, 8):
-    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
-    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
-else:
-    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
+"""Utilities for data science and machine learning."""
+from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
 
 try:
     # Change here if project is renamed and does not equal the package name
@@ -16,12 +11,12 @@ finally:
     del version, PackageNotFoundError
 
 
-from gouda.binaryconfusionmatrix import BinaryConfusionMatrix  # noqa: F401
-from gouda.confusionmatrix import ConfusionMatrix  # noqa: F401
-from gouda.constants import *  # noqa: F401, F403
-from gouda.data_methods import *  # noqa: F401, F403
-from gouda.display import print_grid, print_image  # noqa: F401
-from gouda.file_methods import *  # noqa: F401, F403
-from gouda.general import *  # noqa: F401, F403
-from gouda.goudapath import GoudaPath  # noqa: F401
-from gouda.moving_stats import *  # noqa: F401, F403
+from gouda.binaryconfusionmatrix import BinaryConfusionMatrix
+from gouda.confusionmatrix import ConfusionMatrix
+from gouda.constants import *
+from gouda.data_methods import *
+from gouda.display import print_grid, print_image, squarify
+from gouda.file_methods import *
+from gouda.general import *
+from gouda.goudapath import GoudaPath
+from gouda.moving_stats import *
